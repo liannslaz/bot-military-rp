13ca0d3bab328fd4d406a848dc00f7cecd790692
liannslaz <lian123lopmama@gmail.com>
2026-09-18 15:49:21 -0400

    Create ids_existentes.py

---
+++ ids_existentes.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# IDs de canales que YA EXISTEN en el servidor y deben reutilizarse, nunca duplicarse.
+
+CANAL_REGLAS_ID = 1550581766874726583
+CANAL_MODERADOR_ID = 1550581766874726587
